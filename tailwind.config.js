b0f9f079bea8ecf272ff6f0d9464f1b33f716a0b
enryu-maker <akifkhan60067@gmail.com>
2024-06-10 14:36:51 +0530

    initial

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+const withMT = require("@material-tailwind/react/utils/withMT");
+module.exports = withMT({
+  content: ["./src/**/*.{js,jsx,ts,tsx}",],
+  theme: {
+    extend: {
+      fontFamily : {
+        Poppins : 'Poppins'
+      },
+      colors : {
+        primary : "#FF4F00"
+      }
+    },
+  },
+  plugins: [],
+})
+
