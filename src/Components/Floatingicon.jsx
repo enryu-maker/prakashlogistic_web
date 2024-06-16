5c88b448bbf9756b912f8a83fd5c99ab703cc6fc
tejastejale <tejastejale13@gmail.com>
2024-06-16 19:21:20 +0530

    spring

--- src/Components/Floatingicon.jsx
+++ src/Components/Floatingicon.jsx
@@ -1,18 +1,44 @@
+import React from 'react';
+import { motion, useMotionValue, useSpring } from 'framer-motion';
 import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 
 const FloatingButton = () => {
+  // Initialize motion values for x and y coordinates
+  const x = useMotionValue(10);
+  const y = useMotionValue(10);
+
+  // Configure spring properties for smooth animation
+  const springConfig = { stiffness: 500, damping: 15 };
+  const springX = useSpring(x, springConfig);
+  const springY = useSpring(y, springConfig);
+
+  // Function to reset the button to its original position
+  const resetPosition = () => {
+    x.set(0);
+    y.set(0);
+  };
+
   return (
-    <>
     <div className='flex flex-col justify-center align-middle w-full h-full bg-black'>
-      <a
-      href="https://wa.me/7773972197" 
-      className="fixed bottom-4 z-50 right-4 bg-green-500 rounded-full p-4 shadow-lg hover:bg-green-600 transition-colors duration-300"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-     <WhatsAppIcon className='text-white scale-150'/>
-    </a></div>
-    </>
+      <motion.div
+        className="fixed bottom-4 right-4 z-50"
+        style={{ x: springX, y: springY }}
+        drag
+        dragElastic={0.7}
+        dragConstraints={{ top: 0, bottom: 0, left: 0, right: 0 }}
+        whileTap={{ scale: 1.2 }}
+        onDragEnd={() => resetPosition()}
+      >
+        <a
+          href="https://wa.me/7773972197"
+          className="bg-green-500 rounded-full p-4 shadow-lg hover:bg-green-600 transition-colors duration-300"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <WhatsAppIcon className='text-white scale-150' />
+        </a>
+      </motion.div>
+    </div>
   );
 };
 
